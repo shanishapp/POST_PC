a9f23aca6fcbd0500c5b1dd1a3f5359a630270ac
shani shapp <shanishapp@gmail.com>
2020-04-16 17:05:22 +0300

    Finished work on ex2, documentation needed

--- app/src/main/java/com/example/postpc/MainActivity.java
+++ app/src/main/java/com/example/postpc/MainActivity.java
@@ -4,26 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
+import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
-
+import android.widget.Toast;
+import com.google.android.material.snackbar.Snackbar;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TodoAdapter.OnTodoListener {
 
     public TextView textView;
     public String text = "Edit Text";
     public EditText editText = null;
     public String editT = "";
     public Button button = null;
+    public ArrayList<TODO> todoList = new ArrayList<>();
     TODO todo = null;
+    private View view = null;
+    private String error_message = "you can't create an empty TODO item, oh silly!";
+    private int duration = Snackbar.LENGTH_SHORT;
+    private Snackbar snackbar = null;
 
 
     @Override
@@ -31,46 +39,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+
         if (savedInstanceState != null) {
             text = savedInstanceState.getString("text");
             editT = savedInstanceState.getString("editT");
-
+            editText.setText(editT);
+            todoList = savedInstanceState.getParcelableArrayList("todolist");
         }
+        snackbar = Snackbar.make(view, error_message, duration);
         editText = findViewById(R.id.editText);
         button = findViewById(R.id.button);
         editText.setText(editT);
         setOrientation();
-        editText.setText(editT);
 
 
-        final List<TODO> todoList = new ArrayList<>();
 
+        final TodoAdapter adapter = new TodoAdapter(todoList,this);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 text = editText.getText().toString();
-                if (text == "") {
-                    //not good
-                }
-                todo = new TODO(text);
-                todoList.add(todo);
-                editText.setText("");
 
+                if (text.equals("")) {
+                    snackbar.show();
+                } else {
+                    todo = new TODO(text, 0);
+                    todoList.add(todo);
+                    editText.setText("");
+                    adapter.notifyDataSetChanged();
+                }
             }
         });
-
-        //enable press create when input keyboard on screen
-
-
-        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
 
 
         // Lookup the recyclerview in activity layout
         RecyclerView rvContacts = (RecyclerView) findViewById(R.id.todo_recycler_view);
 
-        // Create adapter passing in the sample user data
-        TodoAdapter adapter = new TodoAdapter(todoList);
         // Attach the adapter to the recyclerview to populate items
         rvContacts.setAdapter(adapter);
         // Set layout manager to position the items
@@ -100,8 +107,24 @@ public class MainActivity extends AppCompatActivity {
         // killed and restarted.
         savedInstanceState.putString("text",text);
         savedInstanceState.putString("editT",editT);
-
+        savedInstanceState.putParcelableArrayList("todolist",todoList);
+        // etc.
     }
 
+    @Override
+    public void onTodoClick(int pos, ImageView imageView) {
+        TODO todoItem = todoList.get(pos);
+        if (todoItem.isDone == 0)
+        {
+            todoItem.isDone = 1;
+            imageView.setBackgroundResource(R.drawable.done);
+            Context context = getApplicationContext();
+            CharSequence text = "TODO "+todoItem.description+" is now DONE. BOOM!";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+    }
 
 }
--- app/src/main/java/com/example/postpc/TODO.java
+++ app/src/main/java/com/example/postpc/TODO.java
@@ -1,12 +1,44 @@
 package com.example.postpc;
 
-public class TODO {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class TODO implements Parcelable {
 
     public String description;
-    public Boolean isDone = false;
+    public int isDone;
 
-    TODO(String d)
-    {
+    TODO(String d, int i) {
         description = d;
+        isDone = i;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(description);
+        dest.writeInt(isDone);
+
+    }
+
+    public static final Parcelable.Creator<TODO> CREATOR
+            = new Parcelable.Creator<TODO>() {
+        public TODO createFromParcel(Parcel in) {
+            return new TODO(in);
+        }
+
+        public TODO[] newArray(int size) {
+            return new TODO[size];
+        }
+    };
+
+    private TODO(Parcel in)
+    {
+        description = in.readString();
+        isDone = in.readInt();
     }
 }

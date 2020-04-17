6eeee96c2795b8bdf9de2132dafb2fc390ad711f
shani shapp <shanishapp@gmail.com>
2020-04-17 15:54:47 +0300

    ex2

--- app/src/main/java/com/example/postpc/MainActivity.java
+++ app/src/main/java/com/example/postpc/MainActivity.java
@@ -4,101 +4,94 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import android.content.Context;
-import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
+
 import com.google.android.material.snackbar.Snackbar;
+
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements TodoAdapter.OnTodoListener {
 
-    public TextView textView;
-    public String text = "Edit Text";
-    public EditText editText = null;
-    public String editT = "";
-    public Button button = null;
-    public ArrayList<TODO> todoList = new ArrayList<>();
-    TODO todo = null;
+    private String text ;
+    private EditText editText = null;
+    private String editTextHint = "";
+    private ArrayList<TODO> todoList = new ArrayList<>();
+    private Button button = null;
+    private TODO todo = null;
     private View view = null;
-    private String error_message = "you can't create an empty TODO item, oh silly!";
-    private int duration = Snackbar.LENGTH_SHORT;
     private Snackbar snackbar = null;
+
+    final private String error_message = "you can't create an empty TODO item, oh silly!";
+    final private int snackbarDuration = Snackbar.LENGTH_SHORT;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-
-        if (savedInstanceState != null) {
-            text = savedInstanceState.getString("text");
-            editT = savedInstanceState.getString("editT");
-            editText.setText(editT);
-            todoList = savedInstanceState.getParcelableArrayList("todolist");
-        }
-        snackbar = Snackbar.make(view, error_message, duration);
-        editText = findViewById(R.id.editText);
-        button = findViewById(R.id.button);
-        editText.setText(editT);
-        setOrientation();
-
-
+        initVariables(savedInstanceState);
 
         final TodoAdapter adapter = new TodoAdapter(todoList,this);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                text = editText.getText().toString();
+                createTodo(adapter);
 
-                if (text.equals("")) {
-                    snackbar.show();
-                } else {
-                    todo = new TODO(text, 0);
-                    todoList.add(todo);
-                    editText.setText("");
-                    adapter.notifyDataSetChanged();
-                }
             }
         });
 
-
-        // Lookup the recyclerview in activity layout
         RecyclerView rvContacts = (RecyclerView) findViewById(R.id.todo_recycler_view);
-
-        // Attach the adapter to the recyclerview to populate items
         rvContacts.setAdapter(adapter);
-        // Set layout manager to position the items
         rvContacts.setLayoutManager(new LinearLayoutManager(this));
-        // That's all!
     }
 
+    private void createTodo(TodoAdapter adapter)
+    {
+        text = editText.getText().toString();
+        if (text.equals("")) {
+            snackbar.show();
+        } else {
+            todo = new TODO(text, 0);
+            todoList.add(todo);
+            editText.setText("");
+            adapter.notifyDataSetChanged();
+        }
+    }
 
+    private void initVariables(Bundle savedInstanceState)
+    {
+        view = findViewById(R.id.main_layout);
+        snackbar = Snackbar.make(view, error_message, snackbarDuration);
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+        setOrientation();
+
+        if (savedInstanceState != null) {
+            text = savedInstanceState.getString("text");
+            editTextHint = savedInstanceState.getString("editTextHint");
+            editText.setText(editTextHint);
+            todoList = savedInstanceState.getParcelableArrayList("todolist");
+        }
+    }
 
     private void setOrientation(){
-        ConstraintLayout view = findViewById(R.id.main_layout);
+        ConstraintLayout view = (ConstraintLayout)findViewById(R.id.main_layout);
         int orientation = getResources().getConfiguration().orientation;
         if(orientation == Configuration.ORIENTATION_LANDSCAPE) {
             view.setBackgroundResource (R.drawable.flowers_landscape);
-        }
-        else {
+        } else {
             view.setBackgroundResource (R.drawable.flowers);
         }
     }
-
-
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
@@ -106,7 +99,7 @@ public class MainActivity extends AppCompatActivity implements TodoAdapter.OnTod
         // This bundle will be passed to onCreate if the process is
         // killed and restarted.
         savedInstanceState.putString("text",text);
-        savedInstanceState.putString("editT",editT);
+        savedInstanceState.putString("editTextHint", editTextHint);
         savedInstanceState.putParcelableArrayList("todolist",todoList);
         // etc.
     }
@@ -121,7 +114,6 @@ public class MainActivity extends AppCompatActivity implements TodoAdapter.OnTod
             Context context = getApplicationContext();
             CharSequence text = "TODO "+todoItem.description+" is now DONE. BOOM!";
             int duration = Toast.LENGTH_SHORT;
-
             Toast toast = Toast.makeText(context, text, duration);
             toast.show();
         }
